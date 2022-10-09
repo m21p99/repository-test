e43b4f48da3fd0f225a33f10e52d360dea494014
m21p99 <krasicjuventus.proka@gmail.com>
2022-10-09 18:53:58 +0200

    Added method Sum.

--- src/main/java/org/example/Calcolatrice.java
+++ src/main/java/org/example/Calcolatrice.java
@@ -1,5 +1,16 @@
 package org.example;
 
+
 public class Calcolatrice {
 
+    public Double Somma(Double x, Double y){
+        return x+y;
+    }
+
+    public static void main(String[] args) {
+        Calcolatrice c = new Calcolatrice();
+
+        System.out.println(c.Somma(5.0, 10.0));
+    }
 }
+
